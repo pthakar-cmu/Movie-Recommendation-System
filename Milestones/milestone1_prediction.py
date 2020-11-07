17c5b94722fb3df76a43b6e00625cf6936bde90d
prathit23 <prathit23@gmail.com>
2020-11-07 14:40:21 -0500

    Updated code for Milestone 2

--- Milestones/milestone1_prediction.py
+++ Milestones/milestone1_prediction.py
@@ -1,71 +1,32 @@
-# from random import randint
-# import pandas as pd
-# from surprise import Reader
-# from surprise import KNNWithMeans
-# from surprise import Dataset
-from datetime import datetime
+
 from random import sample
-import joblib
-import time
-# import re
-import csv
+import pandas as pd
+import json
 import sys
-import time
 
+def validate(test_user):
+  try:
+    int(test_user)
+    return True
+  except:
+    return False
 
+def read_json(name):
+  with open(name) as f:
+    data = json.load(f)
+  return data
 
-#print("--- %s seconds ---" % (time.time() - start_time))
+movie_list = read_json('movie_set.json')
+prediction_data = read_json('final_prediction.json')
 
-#test_user = int(sys.argv[1])
-#test_user = "997661"
-#test_user = rating_user[randint(0,len(rating_user))]
-#print("test user:", test_user)
-
-
-
-result = []
-
-def readFile():
-  return result
-
-def recommend(test_user):
+def recommend(test_user, movie_list, prediction_data):
   result = []
-  start_time = time.time()
-  filename = 'finalized_model.sav'
-  algo = joblib.load(filename)
-
-  start = int(round(time.time() * 1000))
-  with open('training_data.csv', newline='') as f:
-    reader = csv.reader(f)
-    lists = list(reader)
-    rating_user = lists[0]
-    rating_item = lists[1]
-
-  if str(test_user) in rating_user:
-
-    predict_dict = {}
-
-    for ind in range(len(rating_item)):
-      prediction = algo.predict(test_user, rating_item[ind])
-      predict_dict[prediction.est] = rating_item[ind]
-
-    ind = 1
-    for score in sorted(predict_dict, reverse=True):
-      #print("top #", ind)
-      #print("score:", score)
-      #print("movie id:" + predict_dict[score])
-      result.append(predict_dict[score])
-      ind += 1
-      if ind > 20:
-        break
+  if test_user in prediction_data:
+    result = prediction_data[test_user]
   else:
-      result = sample(rating_item,20)
-
-  print("-> ",int(round(time.time() * 1000))-start)
+    result = sample(movie_list,20)
   return ",".join(result)
 
 if __name__ == "__main__":
-  test_user = int(sys.argv[1])
-  recommend(test_user)
-
-#print("--- %s seconds ---" % (time.time() - start_time))w
+  test_user = str(sys.argv[1])
+  recommend(test_user, movie_list, prediction_data)
--- Milestones/web_server.py
+++ Milestones/web_server.py
@@ -1,23 +1,36 @@
 from flask import Flask
-from milestone1_prediction import recommend
+from milestone1_prediction import recommend, validate
+import time
+from milestone1_prediction import read_json
+
 app = Flask(__name__)
 
-
-# @app.route('/h')
-# def index():
-#     return "Hello"
-
 resultGlobal = []
+movie_list = read_json('movie_set.json')
+prediction_data = read_json('final_prediction.json')
 
 @app.route('/recommend/<userid>')
 def index(userid):
 
     global resultGlobal
 
-    return str(recommend(userid, resultGlobal))
+    start_time = round(time.time() * 1000)
+    # Data Quality
+    if not validate(test_user=userid):
+        outputFileTrain.write(",,,Invalid")
+        outputFileTrain.write('\n')
+        return ""
+
+    result = str(recommend(userid, movie_list, prediction_data))
+
+    # Collecting Online Telemetry
+    outputFileTrain.write(str(start_time)+","+userid+","
+                          +str(round(time.time() * 1000) - start_time)+","+result)
+    outputFileTrain.write('\n')
+    return result
 
 if __name__ == '__main__':
-    print("Hey There")
-    resultGlobal = readFile()
-    app.run( host="0.0.0.0", port="8082")
+    outputFileTrain = open("logs.csv", "w")
+    app.run( host="0.0.0.0", port=8082)
+    outputFileTrain.close()
 

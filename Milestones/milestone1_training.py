06492812ec83e57a501cc5afeceb7f71c15e0ab5
Jared Liu <xunliu4533@gmail.com>
2020-11-08 21:14:04 -0500

    Offline evaluation

--- Milestones/milestone1_training.py
+++ Milestones/milestone1_training.py
@@ -7,43 +7,47 @@ import joblib
 import time
 import re
 import csv
+from surprise.model_selection import GridSearchCV, train_test_split
+from model_evaluation import get_metrics_summary, get_top_n, precision_recall_at_k
 
 start_time = time.time()
 
+# Read in data
 rating_user, rating_item, rating = [], [], []
-num_file = 474
+num_file = 3
 
 for ind_file in range(num_file):
-  rating_file_name = "part-"
-  for ind_zero in range(5 - len(str(ind_file))):
-    rating_file_name += "0"
-  rating_file_name += str(ind_file)
-  rating_file = open('KafkaData/userRating/' + rating_file_name, "r")
-  rating_file_content = rating_file.readlines()
-  for ind_line in range(len(rating_file_content)):
-    seg = re.findall(r"'[ a-zA-Z._\-+*0-9]+'", rating_file_content[ind_line])
+    rating_file_name = "part-"
+    for ind_zero in range(5 - len(str(ind_file))):
+        rating_file_name += "0"
+    rating_file_name += str(ind_file)
+    rating_file = open('KafkaData/userRating/' + rating_file_name, "r")
+    rating_file_content = rating_file.readlines()
+    for ind_line in range(len(rating_file_content)):
+        seg = re.findall(r"'[ a-zA-Z._\-+*0-9]+'", rating_file_content[ind_line])
+        try:
+            rating.append(int(seg[2][1:-1]))
+        except:
+            continue
+        try:
+            rating_user.append(int(seg[0][1:-1]))
+        except:
+            rating.pop()
+            continue
+        rating_item.append(seg[1][1:-1])
 
-    try:
-      rating.append(int(seg[2][1:-1]))
-    except:
-      continue
-
-    try:
-        rating_user.append(int(seg[0][1:-1]))
-    except:
-        rating.pop()
-        continue
-    rating_item.append(seg[1][1:-1])
-
-  rating_file.close()
+    rating_file.close()
 
 user_set = list(set(rating_user))
 movie_set = list(set(rating_item))
+print('Totally unique users: ' + str(len(user_set)))
+print('Totally unique movies: ' + str(len(movie_set)))
 
 with open('movie_names.csv', 'w') as f:
     write = csv.writer(f)
     write.writerows([movie_set])
 
+# Prepare dataset
 ratings_dict = {
     "item": rating_item,
     "user": rating_user,
@@ -51,32 +55,59 @@ ratings_dict = {
 }
 
 df = pd.DataFrame(ratings_dict)
+train_df = df.sample(frac=0.8, random_state=0)
+test_df = df.drop(train_df.index)
+
 reader = Reader(rating_scale=(1, 5))
-data = Dataset.load_from_df(df[["user", "item", "rating"]], reader)
-sim_options = {                       # To use user-based cosine similarity
-    "name": "cosine",
-    "user_based": True,
-}
-algo = KNNWithMeans(sim_options=sim_options)
+train_data = Dataset.load_from_df(train_df[["user", "item", "rating"]], reader)
+test_data = Dataset.load_from_df(test_df[["user", "item", "rating"]], reader)
 
-trainingSet = data.build_full_trainset()
-algo.fit(trainingSet)
+# Train models using Grid Search CV
+print('Start parameter search')
+param_grid = {'k': [30, 40, 50]}
+gs = GridSearchCV(KNNWithMeans, param_grid, measures=["rmse"], cv=5)
+gs.fit(train_data)
+print("Best score: ", gs.best_score['rmse'])
+best_params = gs.best_params["rmse"]
 
-final_prediction = {}
-for i in range(len(user_set)):
-  predict_dict = {}
-  for ind in range(len(movie_set)):
-    prediction = algo.predict(user_set[i], movie_set[ind])
-    predict_dict[prediction.est] = movie_set[ind]
-  ind = 1
-  result = []
-  for score in sorted(predict_dict, reverse=True):
-    result.append(predict_dict[score])
-    ind += 1
-    if ind > 20:
-      final_prediction[user_set[i]] = result
-      break
+# Model evaluation
+print('Start model evaluation')
+trainset = train_data.build_full_trainset()
+testset = test_data.build_full_trainset().build_testset()
+sim_options = {"name": "cosine", "user_based": True}
+algo = KNNWithMeans(k=best_params['k'], sim_options=sim_options)
+algo.fit(trainset)
+val_predictions = algo.test(testset)
+metrics = precision_recall_at_k(val_predictions)
+get_metrics_summary(metrics, 'user_info.csv', 'offline_metrics_report_knn.txt', 'precision')
 
-with open('Prediction_data.csv', 'w') as f:
-    for key in final_prediction.keys():
-        f.write("%s, %s\n" % (key, final_prediction[key]))
+# Recommendation lists
+print('Get recommendations')
+full_data = Dataset.load_from_df(df[["user", "item", "rating"]], reader)
+known_data = full_data.build_full_trainset()
+predict_data = known_data.build_anti_testset()
+all_predictions = algo.test(predict_data)
+recommendations = get_top_n(all_predictions, 20)
+rec_df = pd.DataFrame(recommendations).transpose()
+rec_df.to_csv('Prediction_data.csv')
+
+print('Done!')
+
+# final_prediction = {}
+# for i in range(len(user_set)):
+#   predict_dict = {}
+#   for ind in range(len(movie_set)):
+#     prediction = algo.predict(user_set[i], movie_set[ind])
+#     predict_dict[prediction.est] = movie_set[ind]
+#   ind = 1
+#   result = []
+#   for score in sorted(predict_dict, reverse=True):
+#     result.append(predict_dict[score])
+#     ind += 1
+#     if ind > 20:
+#       final_prediction[user_set[i]] = result
+#       break
+
+# with open('Prediction_data.csv', 'w') as f:
+#     for key in final_prediction.keys():
+#         f.write("%s, %s\n" % (key, final_prediction[key]))
---
+++ Milestones/model_evaluation.py
@@ -0,0 +1,117 @@
+from collections import defaultdict
+import pandas as pd
+
+def precision_recall_at_k(predictions, k=20, threshold=3.5, epsilon=1e-7):
+    """
+    Return precision and recall at k metrics for each user. 
+    Modified from https://surprise.readthedocs.io/en/stable/FAQ.html#how-to-compute-precision-k-and-recall-k
+    """
+    print('Calculating metrics')
+    # First map the predictions to each user.
+    user_est_true = defaultdict(list)
+    for uid, _, true_r, est, _ in predictions:
+        user_est_true[uid].append((est, true_r))
+
+    metrics = {'user_id': [], 'precision': [], 'recall': [], 'f1': []}
+    for uid, user_ratings in user_est_true.items():
+        
+        # Sort user ratings by estimated value
+        user_ratings.sort(key=lambda x: x[0], reverse=True)
+
+        # Number of relevant items
+        n_rel = sum((true_r >= threshold) for (_, true_r) in user_ratings)
+
+        # Number of recommended items in top k
+        n_rec_k = sum((est >= threshold) for (est, _) in user_ratings[:k])
+
+        # Number of relevant and recommended items in top k
+        n_rel_and_rec_k = sum(((true_r >= threshold) and (est >= threshold))
+                              for (est, true_r) in user_ratings[:k])
+
+        # Precision@K: Proportion of recommended items that are relevant
+        # When n_rec_k is 0, Precision is undefined. We here set it to 0.
+
+        precision = n_rel_and_rec_k / n_rec_k if n_rec_k != 0 else 0
+
+        # Recall@K: Proportion of relevant items that are recommended
+        # When n_rel is 0, Recall is undefined. We here set it to 0.
+
+        recall = n_rel_and_rec_k / n_rel if n_rel != 0 else 0
+
+        # Add f1 score here
+        f1 = 2 * precision * recall / (precision + recall + epsilon)
+
+        metrics['user_id'].append(uid)
+        metrics['precision'].append(precision)
+        metrics['recall'].append(recall)
+        metrics['f1'].append(f1)
+
+    return metrics
+
+
+def age_group(num):
+    if num <= 20:
+        return '0-20'
+    elif num <= 35:
+        return '21-35'
+    elif num <= 50:
+        return '36-50'
+    else:
+        return '51+'
+
+
+def get_metrics_summary(metrics, user_info, output, score='f1'):
+    if score not in ['f1', 'precision', 'recall']:
+        print('Please choose a valid metric (f1, precision, recall)!')
+    print('Summarizing metrics')
+    users = pd.read_csv(user_info)
+    metrics = pd.DataFrame(metrics)
+    df = pd.merge(metrics, users, how='left', on='user_id')
+    mean_score = df[score].mean()
+    genders = df[['gender', score]].groupby(['gender']).mean().reset_index().to_dict('split')['data']
+    df['age_group'] = df['age'].apply(age_group)
+    ages = df[['age_group', score]].groupby(['age_group']).mean().reset_index().to_dict('split')['data']
+    write_offline_metrics_report(score, mean_score, genders, ages, output)
+
+
+def write_offline_metrics_report(score, mean_score, gender_score, age_score, output):
+    with open(output, 'w') as file:
+        file.write('********** Offline Metric Report **********')
+        file.write('\n\n')
+        file.write('Mean ' + score + ' Score: ' + str(mean_score))
+        file.write('\n\n')
+        file.write('Group by Gender\n')
+        for row in gender_score:
+            file.write(row[0] + ': \t' + str(row[1]) + '\n')
+        file.write('\n')
+        file.write('Group by Age\n')
+        for row in age_score:
+            file.write(row[0] + ': \t' + str(row[1]) + '\n')
+        file.write('\n')
+
+
+def get_top_n(predictions, n=20):
+    """
+    Return the top-N recommendation for each user from a set of predictions.
+    Modified from https://surprise.readthedocs.io/en/stable/FAQ.html#how-to-get-the-top-n-recommendations-for-each-user
+
+    Args:
+        predictions(list of Prediction objects): The list of predictions, as returned by the test method of an algorithm.
+        n(int): The number of recommendation to output for each user. Default is 20.
+
+    Returns:
+    A dict where keys are user (raw) ids and values are lists of tuples:
+        [(raw item id, rating estimation), ...] of size n.
+    """
+
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = [r[0] for r in user_ratings[:n]]
+
+    return top_n

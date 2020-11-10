298896e4c94e6629492bba9ad8f961a2e07d5c8a
Xingchen Wang <55593466+wangxingchen2930@users.noreply.github.com>
2020-11-09 23:47:43 -0500

    Update and rename test_recommendation_api copy.py to test_recommendation_api.py

--- CI/test_recommendation_api.py
+++ CI/test_recommendation_api.py
@@ -4,19 +4,16 @@ import requests
 def test_prediction_status():
 
         response = requests.get('http://128.2.205.5:8082/recommend/11880')
-        print(response.status_code == 200)
 
         assert response.status_code == 200
 
 def test_prediction():
         old_list1 = requests.get('http://128.2.205.5:8082/recommend/11880')
         old_list2 = requests.get('http://128.2.205.5:8082/recommend/11880')
-        print(old_list1.content == old_list2.content)
 
         assert old_list1.content == old_list2.content
 
         new_list1 = requests.get('http://128.2.205.5:8082/recommend/12345')
         new_list2 = requests.get('http://128.2.205.5:8082/recommend/12345')
-        print(new_list1.content != new_list2.content)
 
         assert new_list1.content != new_list2.content
